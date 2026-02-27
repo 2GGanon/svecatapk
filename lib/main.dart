67d45b783c979e55628941884aacbb5304f814c8
2GGanon <stephen.tsuji@gmail.com>
2026-02-27 00:53:25 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SveCatalogueApp());
+}
+
+class SveCatalogueApp extends StatelessWidget {
+  const SveCatalogueApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SVE Catalogue',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.teal, useMaterial3: true),
+      home: const CatalogueWebViewPage(),
+    );
+  }
+}
+
+class CatalogueWebViewPage extends StatefulWidget {
+  const CatalogueWebViewPage({super.key});
+
+  @override
+  State<CatalogueWebViewPage> createState() => _CatalogueWebViewPageState();
+}
+
+class _CatalogueWebViewPageState extends State<CatalogueWebViewPage> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) {
+            if (mounted) setState(() => _isLoading = false);
+          },
+        ),
+      )
+      ..loadFlutterAsset('assets/www/index.html');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (_isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
